e99c746ac47a4108b945a9eed18a83f95eec5444
Aaron <parisi.aaron@gmail.com>
2021-03-25 23:56:16 +0000

    apply what we learned + largest prime + palindrome extra credit

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -40,8 +40,10 @@ describe("About Applying What We Have Learnt", function() {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+      productsICanEat = products.filter(prod => ! prod.containsNuts)
+        .filter(noNuts => _(noNuts.ingredients).all(ingredient => ingredient !== "mushrooms"))
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -55,14 +57,24 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = _.range(1000)
+      .reduce((sum, curVal) => {
+        if (
+          (curVal % 3 === 0) ||
+          (curVal % 5 === 0)
+        ) {
+          return sum + curVal
+        } else {
+          return sum;
+        }
+      })
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -75,25 +87,116 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
 
     /* chain() together map(), flatten() and reduce() */
+    ingredientCount = _(products).chain()
+      .map(prod => prod.ingredients)
+      .flatten()
+      .reduce((accumArr, ingredient) => {
+        accumArr[ingredient] = (accumArr[ingredient] || 0) + 1
+        return accumArr;
+      }, ingredientCount)
+      .value()
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
   /* UNCOMMENT FOR EXTRA CREDIT */
-  /*
+  
   it("should find the largest prime factor of a composite number", function () {
+    const isPrime = num => {
+      for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+          if(num % i === 0) return false; 
+      return num > 1;
+    }
 
+    const largestPrime = num => {
+      let largestPrime = 1;
+
+      largestPrime = _.range(num/2 + 1)
+      .reduce((curLargest, curItem) => {
+        return (isPrime(curItem) && (num % curItem === 0)) ? curItem : curLargest
+      }, largestPrime)
+
+      return largestPrime;
+    }
+
+    expect(largestPrime(10)).toBe(5)
+    expect(largestPrime(2)).toBe(1)
+    expect(largestPrime(12)).toBe(3)
   });
 
   it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
+    const isPalindrome = (arr) => {
+      for (let i=0; i<arr.length/2; i++) {
+        if (arr[i] !== arr[arr.length-1-i]) {
+          return false
+        }
+      }
+
+      return true
+    }
+
+    const getRest = (arr, toExclude) => {
+      // given an array and an index,
+      // returns an array which contains all elements of the given array
+      // minus the element at the given index
+      return arr.filter((el, idx) => idx !== toExclude)
+    }
+
+    const permsOfLength = (len, arr) => {
+      let ret = [];
+
+      if (len === 1) {
+        // base case; return arrays of length 1
+        for (let i=0; i<arr.length; i++) {
+          ret.push([arr[i]])
+        }
+
+        return ret;
+      } else {
+        // a perm of length n consists of a single element of the array in position 0
+        // plus all permutations of length n-1 of the "rest" of the array
+        for (let i=0; i<arr.length; i++) {
+          const curPerms = permsOfLength(len-1, getRest(arr, i))  // get all smaller perms
+          curPerms.forEach(smallerPerm => smallerPerm.unshift(arr[i]))  // put current 'first' element at the front
+
+          ret = ret.concat(curPerms)
+        }
+
+        return ret
+      }
+    }
+
+    const genAllPermutations = (arr) => {
+      let allPerms = [];
+      
+      for (let i=1; i<=arr.length; i++) {
+        // generate all permutations of length i
+        allPerms = allPerms.concat(permsOfLength(i, arr))
+      }
+      
+      return allPerms
+    }
+
+    const largestPalindrome = (a, b) => {
+      const digits = (a*b).toString().split("").map(digitString => parseInt(digitString))
+      const candidates = genAllPermutations(digits)    // get all permutations
+        .filter(candidate => isPalindrome(candidate))  // get palindromes
+        .map(pal => parseInt(pal.join("")))            // turn them back into numbers
+        .sort((a, b) => a-b)                           // sort by number, ascending
+      
+      return parseInt(candidates[candidates.length - 1])
+    }
+
+    // expect(largestPalindrome(112, 154)).toBe(8)
+    expect(largestPalindrome(123, 456)).toBe(868)  // prod = 56088
 
   });
 
@@ -109,5 +212,5 @@ describe("About Applying What We Have Learnt", function() {
   it("should find the 10001st prime", function () {
 
   });
-  */
+  
 });
